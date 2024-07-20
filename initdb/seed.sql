7c981c7ccffec33ad8d005019b6d5f6e6bf74035
Yuto Taniguchi <youdougukou5@gmail.com>
2024-07-20 23:52:46 +0900

    なんか違う

---
+++ initdb/seed.sql
@@ -0,0 +1,12 @@
+-- seed.sql
+
+USE anki_db;
+
+-- USERSテーブルにデータを挿入
+INSERT INTO USERS (username, email, password) VALUES ('demo_user', 'demo@example.com', 'password123');
+
+-- DECKSテーブルにデータを挿入
+INSERT INTO DECKS (user_id, deck_name) VALUES (1, 'My First Deck');
+INSERT INTO DECKS (user_id, deck_name) VALUES (1, '単語帳');
+
+-- その他のテーブルに必要な初期データを挿入
