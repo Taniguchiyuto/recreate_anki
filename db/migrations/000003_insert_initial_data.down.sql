2672a280ecf1364db4f0a92734879735303ece74
Yuto Taniguchi <youdougukou5@gmail.com>
2024-07-24 02:48:39 +0900

    reset to sion 😢

--- db/migrations/000003_insert_initial_data.down.sql
+++ db/migrations/000003_insert_initial_data.down.sql
@@ -5,7 +5,4 @@ DELETE FROM DECKS WHERE deck_name IN ('My First Deck', 'うんこ単語帳');
 DELETE FROM USERS WHERE username = 'demo_user';
 
 -- cardsテーブルから初期データを削除
-DELETE FROM cards WHERE nid IN (SELECT id FROM notes WHERE guid = 'unique-guid-12345');
 
--- notesテーブルから初期データを削除
-DELETE FROM notes WHERE guid = 'unique-guid-12345';
