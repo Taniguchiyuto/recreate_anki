5f017cedc9590848bc2c07c63d65ce0550c600cb
Yuto Taniguchi <youdougukou5@gmail.com>
2024-07-24 18:47:41 +0900

    test

---
+++ db/migrations/000004_recreate_decks_table.down.sql
@@ -0,0 +1,5 @@
+-- NOTESテーブルを削除
+DROP TABLE IF EXISTS notes;
+
+-- CARDSテーブルを削除
+DROP TABLE IF EXISTS cards;
---
+++ db/migrations/000004_recreate_decks_table.up.sql
@@ -0,0 +1,38 @@
+-- NOTESテーブルを作成
+CREATE TABLE notes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    guid VARCHAR(255) NOT NULL,
+    mid INT NOT NULL,
+    `mod` INT NOT NULL,  -- 'mod'は予約語のためバッククォートで囲む
+    usn INT NOT NULL,
+    tags TEXT,
+    flds TEXT,
+    sfld TEXT,
+    csum INT NOT NULL,
+    flags INT,
+    data TEXT
+);
+
+-- CARDSテーブルを作成
+CREATE TABLE cards (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nid INT NOT NULL,
+    did INT NOT NULL,
+    `ord` INT NOT NULL,  -- 'ord'は予約語のためバッククォートで囲む
+    `mod` INT NOT NULL,  -- 'mod'は予約語のためバッククォートで囲む
+    usn INT NOT NULL,
+    `type` INT NOT NULL,  -- 'type'は予約語のためバッククォートで囲む
+    queue INT NOT NULL,
+    due INT NOT NULL,
+    ivl INT,
+    factor INT,
+    reps INT,
+    lapses INT,
+    `left` INT,  -- 'left'は予約語のためバッククォートで囲む
+    odue INT,
+    odid INT,
+    flags INT,
+    data TEXT,
+    FOREIGN KEY (nid) REFERENCES notes(id),
+    FOREIGN KEY (did) REFERENCES decks(id)
+);
