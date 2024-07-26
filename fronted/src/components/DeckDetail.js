770e95fd8c8d38f1d6638a2abf701cd77b8aa345
Yuto Taniguchi <youdougukou5@gmail.com>
2024-07-26 17:12:38 +0900

    dbデモ要素フルセット🤞

--- fronted/src/components/DeckDetail.js
+++ fronted/src/components/DeckDetail.js
@@ -1,26 +1,26 @@
-// import React, { useState, useEffect } from "react";
-// import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-// function DeckDetail() {
-//   const { id } = useParams(); // デッキIDを取得
-//   const [cards, setCards] = useState([]);
+function DeckDetail() {
+  const { id } = useParams(); // デッキIDを取得
+  const [cards, setCards] = useState([]);
 
-//   useEffect(() => {
-//     fetch(`http://api-server-domain:8000/api/deck/${id}/cards`) // APIエンドポイントにリクエストを送信
-//       .then((response) => response.json())
-//       .then((data) => setCards(data));
-//   }, [id]);
+  useEffect(() => {
+    fetch(`http://api-server-domain:8000/api/deck/${id}/cards`) // APIエンドポイントにリクエストを送信
+      .then((response) => response.json())
+      .then((data) => setCards(data));
+  }, [id]);
 
-//   return (
-//     <div>
-//       <h1>Deck {id}</h1>
-//       <ul>
-//         {/* {cards.map((card) => (
-//         //   <li key={card.id}>{card.id}</li> // カードの内容を表示
-//         ))} */}
-//       </ul>
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <h1>Deck {id}</h1>
+      <ul>
+        {cards.map((card) => (
+          <li key={card.id}>{card.id}</li> // カードの内容を表示
+        ))}
+      </ul>
+    </div>
+  );
+}
 
-// export default DeckDetail;
+export default DeckDetail;
