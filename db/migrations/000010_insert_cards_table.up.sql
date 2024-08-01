0ffcb34593772b45d93845900ac1453e1614db33
Yuto Taniguchi <youdougukou5@gmail.com>
2024-08-01 23:10:02 +0900

    復習間隔実装前🔥

---
+++ db/migrations/000010_insert_cards_table.up.sql
@@ -0,0 +1,11 @@
+INSERT INTO notes (guid, mid, `mod`, usn, tags, flds, sfld, csum, flags, data)
+VALUES 
+('new-guid-unique', 2, 1627912195, 2, 'new,tag', 'New Question Content,New Answer Content', 'New Question Content', 123456789, 0, 'New extra data for note');
+
+
+
+INSERT INTO cards (nid, did, `ord`, `mod`, usn, `type`, queue, due, ivl, factor, reps, lapses, `left`, odue, odid, flags, data)
+VALUES 
+(5, 1, 0, 1627912195, 2, 0, 0, 1627913195, 0, 2500, 0, 0, 0, 0, 0, 0, 'Card data for note 5 in deck 1');
+
+
